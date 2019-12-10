83da1e5c4a24136cea9675a96d101e3e3ea6e3f0
d-bcarrue <diego@laptop.localdomain>
2019-12-10 19:46:14 +0100

    Cambios menores en TODOs y formato

--- Data_transform.py
+++ Data_transform.py
@@ -28,17 +28,17 @@ FILE_NAME="Reef_Life_Survey_(RLS)#_Global_reef_fish_dataset-fish_surveys.csv"
 
 
 
-# In[4]: TO-DO -> Leer el fichero en 64 particiones y en una sola línea
+# In[4]: DONE -> Leer el fichero en 64 particiones y en una sola línea
 file = sc.textFile(FILE_NAME, minPartitions=64)
 
 
-# In[5]: TO-DO -> Leer a una lista las columnas que aparezcan en la primera línea del fichero .csv
+# In[5]: DONE -> Leer a una lista las columnas que aparezcan en la primera línea del fichero .csv
 columnas = file.take(1)[0].split(',')
 
 print(columnas)
 
 
-# In[6]: TO-DO -> Contruir el RDD a partir del conjunto de datos, separando por columnas y eliminando la cabecera
+# In[6]: DONE -> Contruir el RDD a partir del conjunto de datos, separando por columnas y eliminando la cabecera
 # Consejo: Utilizar el método "arreglaComas", que codifica las comas que aparecen en cadenas literales y, por tanto, no separan campos. Tras utilizar el método, los campos se pueden obtener separando por ",".
 
 def arreglaComas(x):
@@ -64,7 +64,7 @@ print("TOTAL ROWS: %d"%rddFilas.count())
 
 
 
-# In[7]: TO-DO -> Transformar el RDD de filas a las columnas que nos interesa: SurveyID, SiteLat, SiteLong, Family y Total
+# In[7]: DONE -> Transformar el RDD de filas a las columnas que nos interesa: SurveyID, SiteLat, SiteLong, Family y Total
 col_idx = []
 col_idx.append(columnas.index("SurveyID"))
 col_idx.append(columnas.index("SiteLat"))
@@ -92,7 +92,7 @@ def toVector(familyCountList):
         return counts
     return counts/total
 
-# TO-DO -> Agrupar las tuplas construidas anteriormente por localización (SurveryID, SiteLat, SiteLong) 
+# DONE -> Agrupar las tuplas construidas anteriormente por localización (SurveryID, SiteLat, SiteLong) 
 # Pista: Utilizar el método toVector
 rddVectors=rddProcesado.map(lambda x: ((x[0],x[1],x[2]),(x[3],x[4])))\
 	.groupByKey().map(lambda x: (x[0],toVector(x[1])))
@@ -114,7 +114,7 @@ def pasaFilaARow(x):
     d["features"]=Vectors.dense(x[2].tolist()+[float(x[0])])
     return Row(**d)
 
-# TO-DO -> Conversión de RDD a DataFrame
+# DONE -> Conversión de RDD a DataFrame
 # Pista: método toDF
 
 dfVectors=rddVectors.map(lambda x: pasaFilaARow((x[0][1],x[0][2],x[1]))).toDF()
@@ -125,6 +125,8 @@ from pyspark.ml.feature import VectorIndexer
 from pyspark.ml.evaluation import RegressionEvaluator
 
 # Análisis preliminar de datos
+#TODO: Mostrar numero de registros, surveys, localizaciones, especies y familias.
+
 # Esquema:
 dfVectors.printSchema()
 # Nº de filas
@@ -143,26 +145,17 @@ dfVectors.select('longitude').distinct().count()
 # dfProcesado.describe().show()
 
 
-# In[10]: TO-DO -> Entrenamiento del modelo de Regresión a través del algoritmo Random forest regression de MLLib
+# In[10]: DONE -> Entrenamiento del modelo de Regresión a través del algoritmo Random forest regression de MLLib
 # Partición del conjunto de datos: 70% Training - 30% Test
 
 (trainingData, testData) = dfVectors.randomSplit([0.7,0.3])
 
 for label in ["latitude", "longitude"]:
 
-	# Train a RandomForest model.
 	rf = RandomForestRegressor(featuresCol="features", labelCol=label)
-
-	# Train model.
 	model = rf.fit(trainingData)
-
-	# Make predictions.
 	predictions = model.transform(testData)
-
-	# Select example rows to display.
 	predictions.select("prediction", label, "features").show(5)
-
-	# Select (prediction, true label) and compute test error
 	evaluator = RegressionEvaluator(
 	    labelCol=label, predictionCol="prediction", metricName="rmse")
 	rmse = evaluator.evaluate(predictions)
